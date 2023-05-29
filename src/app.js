3109e1c8e4e078edd53523f07b421cb172be9e32
Angel Sanabria <angeleliusanabria@hotmail.com>
2023-05-29 07:35:07 -0500

    push

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ app.use('/', (req, res) => {
     res.send('Hola desde el Servidor.');
 });
 
-app.use('/', (req, res) => {
+app.use('/api', (req, res) => {
     res.send('Hola desde la API.');
 });
 
