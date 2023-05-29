c99347bcb45ec30f1eafa86e51d860d06d802659
Angel Sanabria <angeleliusanabria@hotmail.com>
2023-05-29 07:16:03 -0500

    push

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const express = require("express");
-const app = express();
-
-const port = process.env.PORT || 4000;
-
-app.listen(port);
-
-app.use('/', (req, res) => {
-    res.send('Hola desde el servidor!');
-});
-
-console.log(`Servidor en puerto ${port}`);
+const index = require("server.js");
