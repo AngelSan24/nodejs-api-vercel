d523c228d07649c2de6692e45a2703c29c890049
Angel Sanabria <angeleliusanabria@hotmail.com>
2023-05-29 07:47:16 -0500

    push

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express"); // Requerir modulo
 const app = express(); // Iniciar app
 
 app.use('/', (req, res) => {
-    res.send('<table>    <tr>      <th>Company</th>      <th>Contact</th>      <th>Country</th>    </tr>    <tr>      <td>Alfreds Futterkiste</td>      <td>Maria Anders</td>      <td>Germany</td>    </tr>    <tr>      <td>Centro comercial Moctezuma</td>      <td>Francisco Chang</td>      <td>Mexico</td>    </tr>  </table>');
+    res.send('Hola desde el Servidor.');
 });
 
 module.exports = app;
